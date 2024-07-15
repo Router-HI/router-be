0ef7e507c6ee42e7232318b46fcd1602596f76e2
dl-00-e8 <sso07012@gmail.com>
2024-07-15 20:42:45 +0900

    chore: router backend project setting

---
+++ router-be/src/main/java/org/spring/routerbe/RouterBeApplication.java
@@ -0,0 +1,13 @@
+package org.spring.routerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouterBeApplication.class, args);
+    }
+
+}
---
+++ router-be/src/test/java/org/spring/routerbe/RouterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.routerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
